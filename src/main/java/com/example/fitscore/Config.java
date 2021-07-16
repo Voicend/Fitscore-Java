531ac93c889f2d2c79b72ce595ed40f8cda2ab37
音尘丶 <voicend.yc@gmail.com>
2021-07-16 13:40:13 +0800

    2021/7/13

--- src/main/java/com/example/fitscore/Config.java
+++ src/main/java/com/example/fitscore/Config.java
@@ -2,7 +2,6 @@ package com.example.fitscore;
 
 public class Config {
     public static String COMatrixfilename = "data/ChangeOverMatrix.csv";
-    public static String inputfilename = "data/input.csv";
     public static String MachineClockMatrixfilename = "data/MachineClockMatrix.csv";
     public static String ModelProcessMatrixfilename = "data/ModelProcessesMatrix.csv";
     public static String ProductlineMatrixfilename = "data/ProductLineMatrix.json";
